972d9c4d95730c0ebc1e50a1f0307b32a9983885
Andrew Hird <andrew.james.hird@gmail.com>
2016-08-03 22:17:26 +1200

    checkForWin working

--- minesweeper.js
+++ minesweeper.js
@@ -21,29 +21,40 @@ var board = {
     {row: 0, col: 1, isMine: false, hidden: true},
     {row: 0, col: 2, isMine: true, hidden: true},
     {row: 1, col: 0, isMine: false, hidden: true},
-    {row: 1, col: 1, isMine: true, hidden: true},
+    {row: 1, col: 1, isMine: false, hidden: true},
     {row: 1, col: 2, isMine: false, hidden: true},
     {row: 2, col: 0, isMine: true, hidden: true},
     {row: 2, col: 1, isMine: false, hidden: true},
-    {row: 2, col: 2, isMine: true, hidden: true}
+    {row: 2, col: 2, isMine: false, hidden: true}
   ]
 }
 
 function startGame () {
 
-  for(var i = 0; i < board.cells.length; i++) {
-      board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
-  }
+for (var i = 0; board.cells[i]; i++) {
+  board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
+}
 
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
+
+  document.addEventListener('click', checkForWin)
+  document.addEventListener('contextmenu', checkForWin)
 }
 
 // Define this function to look for a win condition:
 //
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
+
 function checkForWin () {
+  for (var i = 0; i < board.cells.length; i++) {
+    if (board.cells[i].isMine === true && board.cells[i].isMarked === true) {
+      if (board.cells[i].isMarked === true && board.cells[i].hidden === true) {
+        lib.displayMessage('You win!')
+      }
+    }
+  }
 
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
@@ -62,10 +73,10 @@ function countSurroundingMines (cell) {
   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
 
   var count = 0
-  for (var i = 0; i < surrounding.length; i++) {
-    if (surrounding[i].isMine === true) {
-      count += 1
-    }
+for (var i = 0; i < surrounding.length; i++) {
+  if (surrounding[i].isMine === true) {
+    count += 1
   }
-  return count
+}
+return count
 }
